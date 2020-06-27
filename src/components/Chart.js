9c7916f015c398cdd0b085ddc1d07f095eefc65f
dgentilli <dgentilli@gmail.com>
2020-06-27 19:00:07 +0300

    Chart: Display doughnut chart for global total.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { Doughnut } from "react-chartjs-2";
 
+/**
+ * REFS: https://www.npmjs.com/package/react-chartjs-2
+ * http://jerairrest.github.io/react-chartjs-2/
+ */
+
 const Chart = (props) => {
   const activeCases = props.active ? props.active : null;
   const deaths = props.deaths ? props.deaths : null;
   const recovered = props.recovered ? props.recovered : null;
   console.log("chart variables: ", activeCases, deaths, recovered);
-  return <h3>Chart</h3>;
-  //   return (
-  // <Doughnut data={{labels: ['Active Cases', 'Deaths', 'Recovered'] }, dataSets: [{data: activeCases, label: 'Active Cases'}, {data: recovered, label: 'Recovered'}, {data: deaths, label: 'deaths'}]} />
-  //   )
+  const doughnut = (
+    <Doughnut
+      data={{
+        labels: ["Active", "Recovered", "Deaths"],
+        datasets: [
+          {
+            data: [activeCases, recovered, deaths],
+            label: ["Active", "Recovered", "Deaths"],
+            borderColor: "#ccc",
+            backgroundColor: ["#ffea08", "#24a822", "#e81f10"],
+            fill: true,
+          },
+        ],
+      }}
+    />
+  );
+  return <div>{activeCases ? doughnut : "Loading..."}</div>;
 };
 export default Chart;
